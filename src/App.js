d2c7c5de792130fe5de30a7d33ad40185152c374
Sreenu-y <srinuyelesam123@gmail.com>
2025-08-17 20:55:41 +0530

    newsapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+import { useState } from "react";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar color="#f11946" progress={progress} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                pageSize={6}
+                category="general"
+              />
+            }
+          />
+          <Route
+            path="/business"
+            element={
+              <News
+                setProgress={setProgress}
+                key="business"
+                pageSize={6}
+                category="business"
+              />
+            }
+          />
+          <Route
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                key="entertainment"
+                pageSize={6}
+                category="entertainment"
+              />
+            }
+          />
+
+          <Route
+            path="/health"
+            element={
+              <News
+                setProgress={setProgress}
+                key="health"
+                pageSize={6}
+                category="health"
+              />
+            }
+          />
+          <Route
+            path="/general"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                pageSize={6}
+                category="general"
+              />
+            }
+          />
+
+          <Route
+            path="/science"
+            element={
+              <News
+                setProgress={setProgress}
+                key="science"
+                pageSize={6}
+                category="science"
+              />
+            }
+          />
+
+          <Route
+            path="/sports"
+            element={
+              <News
+                setProgress={setProgress}
+                key="sports"
+                pageSize={6}
+                category="sports"
+              />
+            }
+          />
+
+          <Route
+            path="/technology"
+            element={
+              <News
+                setProgress={setProgress}
+                key="technology"
+                pageSize={6}
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/News.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export default class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 6,
+    category: "general",
+  };
+
+  static propTypes = {
+    category: PropTypes.string,
+    pageSize: PropTypes.number,
+    country: PropTypes.string,
+  };
+
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0,
+    };
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} - NewsMonkey`;
+  }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+  async updateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=bf556b3e40e04c118321d4128916a4f1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.props.setProgress(10);
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.props.setProgress(70);
+    let uniqueArticles = parsedData.articles.filter(
+      (a, index, self) => index === self.findIndex((t) => t.url === a.url)
+    );
+
+    this.setState({
+      articles: uniqueArticles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+    this.props.setProgress(100);
+  }
+
+  fetchMoreData = async () => {
+    this.setState(
+      (prevState) => ({ page: prevState.page + 1 }),
+      async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=bf556b3e40e04c118321d4128916a4f1&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+
+        // Deduplicate before setting
+        let uniqueArticles = parsedData.articles.filter(
+          (a, index, self) => index === self.findIndex((t) => t.url === a.url)
+        );
+
+        this.setState({
+          articles: this.state.articles.concat(uniqueArticles),
+          totalResults: parsedData.totalResults,
+          loading: false,
+        });
+      }
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="text-center" style={{ margin: "30px 0px" }}>
+          {`NewsMonkey Top-Headlines from  ${this.capitalizeFirstLetter(
+            this.props.category
+          )}`}
+        </h1>
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className=" row">
+              {!this.state.loading &&
+                this.state.articles.map((element) => {
+                  return (
+                    <div
+                      className="col-md-4"
+                      key={element.url || element.publishedAt + element.title}
+                    >
+                      <NewsItem
+                        title={element.title ? element.title.slice(0, 44) : ""}
+                        description={
+                          element.description
+                            ? element.description.slice(0, 88)
+                            : ""
+                        }
+                        imageUrl={
+                          element.urlToImage
+                            ? element.urlToImage
+                            : "https://media.cnn.com/api/v1/images/stellar/prod/2012-01-10t120000z-577591873-gm1e81b0hjr01-rtrmadp-3-kodak.JPG?c=16x9&q=w_800,c_fill"
+                        }
+                        newsUrl={element.url}
+                        author={element.author}
+                        publishedAt={element.publishedAt}
+                        source={element.source.name}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    );
+  }
+}
